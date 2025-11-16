1b6550c6531b44ff695195a684d09d045d3c7a2a
gopher <gopher.hu@gmail.com>
2025-11-16 21:36:10 +0100

    Normalize line endings to CRLF

---
+++ TOOLS/BitmapFontGenerator/bitmapfontform.pas
@@ -0,0 +1,37 @@
+unit BitmapFontForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+
+end.
+
